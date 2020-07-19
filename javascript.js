f7c2a2763938b4f20599d6f7d854910157b5f0dc
Alex Madrigal <jam.madrigal@gmail.com>
2020-07-19 13:47:34 -0700

    the basic html and css of the application is in place

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
 
-var cityName = "putusertextherelater";
+var cityName = $("#userCity".text);
 var currentDay = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=1e5b1e82033f913ca953c232c1749468";
 var fiveDay = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=1e5b1e82033f913ca953c232c1749468";
 
-
+$("#cityButton").on("click", function(){
+    console.log(cityName);
+})
 
 
 })
