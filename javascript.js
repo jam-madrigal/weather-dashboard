abc4d91eaef8fca94f20a75f7ba122aacc851244
Alex Madrigal <jam.madrigal@gmail.com>
2020-07-19 14:13:42 -0700

    the button will now log the input field

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 
+    var currentDay = "api.openweathermap.org/data/2.5/weather?q=" + userCity + "&appid=1e5b1e82033f913ca953c232c1749468";
+    var fiveDay = "api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&appid=1e5b1e82033f913ca953c232c1749468";
 
-var cityName = $("#userCity".text);
-var currentDay = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=1e5b1e82033f913ca953c232c1749468";
-var fiveDay = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=1e5b1e82033f913ca953c232c1749468";
+
 
 $("#cityButton").on("click", function(){
-    console.log(cityName);
+    var userCity = $("#userCity").val();
+
+    console.log(userCity);
 })
 
 
